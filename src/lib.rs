f793023fccc332bc5059ff7a9e06dc7118733443
Polluktus <ternka122@gmail.com>
2021-05-09 17:54:42 +0200

    added bytes flag

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,7 @@ Wymiana:      {}           {}     {} "
     println!("{}", tmp);
 }
 
-pub fn human(contains: &String) {
+pub fn mebi(contains: &String) {
     let mem_total = normal(&contains, String::from("MemTotal"));
     let mem_free = normal(&contains, String::from("MemFree"));
     let buffers = normal(&contains, String::from("Buffers"));
@@ -62,5 +62,29 @@ pub fn human(contains: &String) {
 Pamięć:         {}Mi    {}Mi     {}Mi          {}Mi           {}Mi        {}Mi 
 Wymiana:         {}Mi       {}Mi      {}Mi"
     ,mem_total/1024, mem_used/1024, mem_free/1024, shmem/1024, buf_cached/1024, mem_available/1024, swap_total/1024, swap_used/1024, swap_free/1024);
+    println!("{}", tmp);
+}
+
+pub fn bytes(contains: &String) {
+    let mem_total = normal(&contains, String::from("MemTotal"));
+    let mem_free = normal(&contains, String::from("MemFree"));
+    let buffers = normal(&contains, String::from("Buffers"));
+    let sreclaimable = normal(&contains, String::from("SReclaimable"));
+    let cached = normal(&contains, String::from("Cached"));
+    let shmem = normal(&contains, String::from("Shmem"));
+    let mem_available = normal(&contains, String::from("MemAvailable"));
+
+    let swap_total = normal(&contains, String::from("SwapTotal"));
+    let swap_free = normal(&contains, String::from("SwapFree"));
+    let swap_cached = normal(&contains, String::from("SwapCached"));
+    
+    let buf_cached = buffers + sreclaimable + cached;
+    let swap_used = swap_total - swap_free - swap_cached;
+    let mem_used = mem_total - mem_free - buffers - cached - sreclaimable;
+    let tmp = format!("
+                razem       użyte       wolne       dzielone       buf/cache       dostępne
+Pamięć:   {}  {}  {}      {}      {}    {}
+Wymiana:   {}           {}  {}"
+    ,mem_total as i64*1024, mem_used as i64*1024, mem_free as i64*1024, shmem as i64*1024, buf_cached as i64*1024, mem_available as i64*1024, swap_total as i64*1024, swap_used as i64*1024, swap_free as i64*1024);
     println!("{}", tmp);
 }
--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,39 @@ use std::io::Read;
 use freers;
 fn main() {
     let matches = App::new("Program free")
-        .version("0.1")
+        .version("0.2")
         .author("Autor: polluktus")
         .about("Poakzuje zużycie ramu")
         .usage("free [OPCJE]")
-        .arg(Arg::with_name("human")
-            .short("h")
-            .long("human")
+        .arg(Arg::with_name("mebi")
+            .short("m")
+            .long("mebi")
             .multiple(false)
             .required(false)
             .takes_value(false)
-            .help("wyjście czytelne dla człowieka"))
+            .help("wyjście w mebibajtach"))
+        .arg(Arg::with_name("bytes")
+            .short("b")
+            .long("bytes")
+            .multiple(false)
+            .required(false)
+            .takes_value(false)
+            .help("wyjście w bajtach"))
         .get_matches();
-    let human = matches.is_present("human");
+    let mebi = matches.is_present("mebi");
+    let bytes = matches.is_present("bytes");
 
     let mut memf = File::open("/proc/meminfo").unwrap();
     let mut contains = String::new();
     memf.read_to_string(&mut contains).unwrap();
 
-    if human == false {
+    if mebi == false && bytes == false{
         freers::basic(&contains);
+    } else if mebi == true && bytes == false {
+        freers::mebi(&contains);
+    } else if mebi == false && bytes == true {
+        freers::bytes(&contains);
     } else {
-        freers::human(&contains);
+        println!("Zły argument");
     }
 }
